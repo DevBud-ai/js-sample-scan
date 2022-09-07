c3e05fefff990f083425563a3284c51b5335ad83
Rahul V Ramesh <rahulvramesh@hotmail.com>
2022-09-08 01:59:22 +0700

    chane

--- njsscan/exceptions.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf_8 -*-
-"""Exceptions njsscan."""
-
-
-# define Python user-defined exceptions
-class YamlParseError(Exception):
-    """Base class for other exceptions."""
-
-    pass
